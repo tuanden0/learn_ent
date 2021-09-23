f7c1b6e6ce16b4b6d397d059475e179ae068c8aa
tuanden0 <doanhtuan171096@gmail.com>
2021-09-23 09:21:54 +0700

    Add unittest OK case for repository

--- internal/userapis/user/v1/repository_test.go
+++ internal/userapis/user/v1/repository_test.go
@@ -2,9 +2,11 @@ package v1
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"testing"
 
+	"github.com/gogo/protobuf/types"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
 	"github.com/tuanden0/learn_ent/internal/userapis/user/v1/ent"
@@ -21,12 +23,30 @@ func genUser() *ent.User {
 		Username: "test_create",
 		Password: "test123456",
 		Email:    "test6478@localhost.com",
-		Role:     0,
+		Role:     1,
 	}
 
 	u, _ := r.Create(context.Background(), in)
 
 	return u
+}
+
+func genUsers() []*ent.User {
+
+	us := []*ent.User{}
+
+	for i := 0; i < 10; i++ {
+		in := &userv1.CreateRequest{
+			Username: fmt.Sprintf("test_create_%v", i),
+			Password: "test123456",
+			Email:    fmt.Sprintf("test_create_%v@localhost.com", i),
+			Role:     1,
+		}
+		u, _ := r.Create(context.Background(), in)
+		us = append(us, u)
+	}
+
+	return us
 }
 
 func TestMain(m *testing.M) {
@@ -84,4 +104,97 @@ func TestRetrieveOK(t *testing.T) {
 	assert.EqualValues(t, dumpy.Role, u.Role)
 	assert.EqualValues(t, dumpy.Password, u.Password)
 	assert.EqualValues(t, dumpy.ID, u.ID)
+}
+
+func TestUpdateOK(t *testing.T) {
+
+	// Init
+	dumpy := genUser()
+	in := &userv1.UpdateRequest{
+		Id: uint64(dumpy.ID),
+		Password: &types.StringValue{
+			Value: "123456",
+		},
+		Email: &types.StringValue{
+			Value: "updated@local.com",
+		},
+	}
+
+	// Execute
+	u, err := r.Update(context.Background(), in)
+
+	// Assertion
+	assert.Nil(t, err)
+	assert.NotNil(t, u)
+	assert.EqualValues(t, dumpy.ID, u.ID)
+	assert.EqualValues(t, dumpy.Username, u.Username)
+	assert.NotEqualValues(t, dumpy.Password, u.Password)
+	assert.NotEqualValues(t, dumpy.Email, u.Email)
+	assert.EqualValues(t, dumpy.Role, u.Role)
+}
+
+func TestDeleteOK(t *testing.T) {
+
+	// Init
+	dumpy := genUser()
+
+	// Execute
+	err := r.Delete(context.Background(), uint64(dumpy.ID))
+
+	// Assertion
+	assert.Nil(t, err)
+}
+
+func TestListWithEmptyBaseOK(t *testing.T) {
+
+	// Init
+	_ = genUsers()
+	in := &userv1.ListRequest{}
+
+	// Execute
+	us, err := r.List(context.Background(), in)
+
+	// Assertion
+	assert.Nil(t, err)
+	assert.NotNil(t, us)
+	assert.EqualValues(t, 5, len(us))
+}
+
+func TestListWithBaseOK(t *testing.T) {
+
+	// Init
+	genUs := genUsers()
+	in := &userv1.ListRequest{
+		Pagination: &userv1.Pagination{
+			Limit: 5,
+			Page:  1,
+		},
+		Filters: []*userv1.Filter{
+			{
+				Key:    "id",
+				Value:  "2",
+				Method: "=",
+			},
+		},
+		Sort: &userv1.Sort{
+			Key:   "id",
+			IsAsc: true,
+		},
+	}
+
+	// Execute
+	us, err := r.List(context.Background(), in)
+
+	// Assertion
+	assert.Nil(t, err)
+	assert.NotNil(t, us)
+	assert.EqualValues(t, 1, len(us))
+
+	for _, u := range genUs {
+		if u.ID == us[0].ID {
+			assert.EqualValues(t, u.Email, us[0].Email)
+			assert.EqualValues(t, u.Username, us[0].Username)
+			assert.EqualValues(t, u.Password, us[0].Password)
+		}
+	}
 }
